94bf573269da5129bd2abc1e56f0bcbb1928001f
ahujatejas06 <ahujatejas06@gmail.com>
2020-02-24 07:08:42 +0000

    everything

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import getpass
 import sys
 import hashlib
-
+import datetime
+from random import choice
+from random import randint
 student_details = dict()
-
-
 class std_details_class:
     def __init__(self, name, _class, roll_no, section, gender, ph_no, DOB):
         self.name = name
@@ -16,7 +16,7 @@ class std_details_class:
         self.DOB = DOB
 
     def std_details(self):
-        return (self.name, self._class, self.roll_no, self.section, self.gender, self.ph_no, self.DOB)
+        return tuple(self.name, self._class, self.roll_no, self.section, self.gender, self.ph_no, self.DOB)
 
     def edit_name(self, name):
         self.name = name
@@ -41,8 +41,21 @@ class admission_number:
         self.house = house
 
     def get_admn(self):
-        return str(self.std_number) + '\\' + str(self.year) + '\\' + str(self.house)
+        return get3digits_as_str(self.std_number) + '\\' + get2digits_as_str(self.year) + '\\' + str(self.house)
 
+def get3digits_as_str(num):
+  if num < 100 and num >= 10:
+    return ('0'+ str(num))
+  elif num > 0 and num < 10:
+    return ('00' + str(num))
+  else:
+    return str(num)
+
+def get2digits_as_str(num):
+  if num > 0 and num < 10:
+    return ('0' + str(num))
+  else:
+    return str(num)
 
 def encrypt_string(hash_string):
     sha_signature = \
@@ -232,8 +245,37 @@ def add_account():
         DOB = input("Please enter DOB (DDMMYYYY) : ")
         student_details[admn] = std_details_class(name, _class, roll_no, section, gender, ph_no, DOB)
 
-def new_admission():
-    pass
+def new_admission(prev_admn):
+    admn = prev_admn + 1
+    year = str((datetime.datetime.now()).year)
+    house = randint(1,4)
+    print(house)
+    if house == 1:
+        house = 'V'
+    elif house == 2:
+        house = 'G'
+    elif house == 3:
+        house = 'K'
+    else:
+        house = 'D'
+    admn = admission_number(admn, int(year[2:]), house)
+    name = input("Enter name : ")
+    _class = 1
+    sequence = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+    section = choice(sequence)
+    gender = input("Enter gender (F/M/O) : ")
+    while gender not in ['F', 'M', 'O']:
+        print("Please enter a valid gender")
+        gender = input("Enter Gender : ")
+    ph_no = int(input("Enter phone number : "))
+    while ph_no >= 10 ** 10 and ph_no < 10 ** 11:
+        print("Please enter a valid phone number")
+        ph_no = int(input("Enter phone number : "))
+    DOB = input("Please enter DOB (DDMMYYYY) : ")
+    student_details[admn] = std_details_class(name, _class, -1, section, gender, ph_no, DOB)
+    print("The admission number of the student is :", admn.get_admn())
+    print("The section of the student is :", section)
+
 
 def main_menu():
     print("Select an option from below")
@@ -248,8 +290,9 @@ def main_menu():
 
 
 def main():
-    student_details[admission_number(399, 12, 'G')] = std_details_class("Tejas Ahuja", 11, 52, 'A', 'M', 4372874347,
-                                                                        '05112002')
+    prev_admn = 0
+    student_details[admission_number(11, 12, 'G')] = std_details_class("Tejas Ahuja", 11, 52, 'A', 'M', 4372874347,
+                                                                     '05112002')
     passwd = getpass.getpass(prompt='Enter Password : ', stream=None)
     passwd = encrypt_string(passwd)
     if passwd != "2317234be85eaa1c30311c7b1cabed30c7fc5e9491daac527f03f8cfb67b791c":
@@ -270,7 +313,8 @@ def main():
         elif x == 4:
             update_account()
         elif x == 5:
-            new_admission()
+            new_admission(prev_admn)
+            prev_admn += 1
         else:
             print("Please select a valid option")
         print()
